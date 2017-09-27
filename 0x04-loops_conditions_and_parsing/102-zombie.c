242986a9f8e3c3d0e35ae0d79383dc21cef998e6
feliciahsieh <felicia@tixwriteoff.com>
2017-09-27 01:31:43 +0000

    non-working version

---
+++ 0x04-loops_conditions_and_parsing/102-zombie.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * infinite_while -
+ *
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - 
+ *
+ */
+void main()
+{
+}
