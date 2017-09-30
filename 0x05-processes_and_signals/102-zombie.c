4d249013ba9d37c6eaa71faf264ff3504d047671
feliciahsieh <felicia@tixwriteoff.com>
2017-09-30 21:35:41 +0000

    betty

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -22,22 +22,23 @@ int infinite_while(void)
  *
  * Return: None
  */
-int main ()
+int main(void)
 {
 	pid_t child_pid;
 	int i;
 
 	for (i = 0; i < 5; i++)
 	{
-		child_pid = fork ();
+		child_pid = fork();
 		if (child_pid > 0)
 		{
 			printf("Zombie process created, PID: %d\n", child_pid);
 		}
-		else {
+		else
+		{
 			exit(0);
 		}
 	}
 	infinite_while();
-	return 0;
+	return (0);
 }
