309ee1045be1306f5722aec6a57a6566cf404e0c
feliciahsieh <felicia@tixwriteoff.com>
2017-11-16 23:51:52 +0000

    refactored

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,6 +1,5 @@
 # Puppet file to create a file in /tmp
-file { 'touch /tmp/holberton':
-  ensure  => file,
+file { 'create file /tmp/holberton':
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
