8697f5116e2b29255260dc33ade9193986486a53
feliciahsieh <felicia@tixwriteoff.com>
2018-01-02 22:59:29 +0000

    working code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-import json, requests
+"""For a given employee ID, returns TODO list info"""
+import json
+import sys
+import requests
 
-empID = eval(input("Your employee ID? "))
-empName = "Felicia"
-nDone = 1
-nTotal = 5
-title = "testTitle"
-print("Employee {} is done with tasks({}/{}):".format(empName, nDone, nTotal))
-for i in range(nTotal):
-    print("{}".format(title))
+empID = sys.argv[1]
+url = 'https://jsonplaceholder.typicode.com/'
+
+url_empID = url + 'users?id=' + str(empID)
+user = requests.get(url_empID)
+userJson = user.json()
+empName = userJson[0]["name"]
+
+url_TodoCompleted = url + 'todos?userId=' + empID + '&completed=true'
+t = requests.get(url_TodoCompleted)
+tJson = t.json()
+nTodoDone = len(tJson)
+
+url_TodoTotal = url + 'todos?userId=' + empID
+tot = requests.get(url_TodoTotal)
+totJson = tot.json()
+nTodo = len(totJson)
+
+print("Employee {} is done with tasks({}/{}):".format(empName, nTodoDone, nTodo))
+for i in range(nTodoDone):
+    print("\t{}".format(tJson[i]["title"]))
