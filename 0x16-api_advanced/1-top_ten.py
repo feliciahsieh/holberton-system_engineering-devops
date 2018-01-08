94ef79cb48e2d9d1ded77b76a4c04c623d237035
feliciahsieh <felicia@tixwriteoff.com>
2018-01-08 23:20:35 +0000

    first commit with code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-
+"""1-top_ten.py - Query Reddit API & find Top10 subreddits hot posts"""
 import json
 import requests
-import sys
 
-if __name__ == "__main__":
-    """ __main__ """
 
-    def top_ten(subreddit):
-        url = "https://www.reddit.com/dev/api/"
+def top_ten(subreddit):
+    """Return Top10 subreddit hot posts"""
+    baseURL = "https://www.reddit.com/r/"
+    url = baseURL + subreddit + "/hot/.json?limit=10"
+    # https://www.reddit.com/r/programming/hot/.json&limit=10
 
-        urlNew = url + 'users/'
-        return requests.get(urlNew + subreddit).json()
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': '214@holbertonschool.com'
+    }
+    result = requests.get(url, headers=headers)
+
+    if result.status_code == 200:
+        result = result.json()
+        children = result.get('data').get('children')
+        for i in range(10):
+            # print("{}".format(result['data']['children'][i]['data']['title']))
+            title = children[i].get('data').get('title')
+            print("{}".format(title))
+    else:
+        print("None")
