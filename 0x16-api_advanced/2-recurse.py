d60ef2036e71e5e73698ed9d863d3d7979d34b45
feliciahsieh <felicia@tixwriteoff.com>
2018-01-09 02:25:21 +0000

    First version. Still buggy. Archiving

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,48 @@
 #!/usr/bin/python3
-
+"""2-recurse.py - Query Reddit API & find titles of all hot articles"""
 import json
+import pprint
 import requests
-import sys
 
-if __name__ == "__main__":
-    """ __main__ """
 
-    def recurse(subreddit, hot_list=[]):
-        url = "https://www.reddit.com/dev/api/"
+def recurse(subreddit, hot_list=[], params={}):
+    """Find hot article titles using recursion"""
 
-        urlNew = url + 'users/'
-        return requests.get(urlNew + subreddit).json()
+    payload = {}
+    pp = pprint.PrettyPrinter(indent=2)
+
+    baseURL = "https://www.reddit.com/r/"
+    url = baseURL + subreddit + "/hot/.json?limit=100"
+
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': '214@holbertonschool.com'
+    }
+
+    result = requests.get(url, headers=headers, params=params)
+
+    if result.status_code == 200:
+        result = result.json()
+        after = result.get('data').get('after')
+        if after is None:
+            children = result.get('data').get('children')
+            title = []
+            for i in range(100):
+                title.append(children[i].get('data').get('title'))
+                hot_list.append(title)
+                pp.pprint(hot_list)
+            return hot_list
+        else:
+            payload = {"after" : after}
+            pp.pprint(payload)
+            recurse(subreddit, hot_list, payload)
+
+            children = result.get('data').get('children')
+            title = []
+            for i in range(100):
+                title.append(children[i].get('data').get('title'))
+                hot_list.append(title)
+                pp.pprint(hot_list)
+    else:
+        print("None")
+        return
