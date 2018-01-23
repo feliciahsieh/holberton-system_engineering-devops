2ad4facdf56a513b3305499f8b35c43de73978d2
feliciahsieh <felicia@tixwriteoff.com>
2018-01-23 02:28:46 +0000

    remove test file references. Add comment. Ran with puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,8 @@
-exec { 'test.php':
-     command => "sed -i 's/phpp/php/' test.php",
-     path => '/bin',
+# Fix bad file ext .phpp
+file { '/var/www/html/wp-settings.php':
+  ensure  => present
+}
+-> exec { 'replace':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }
