00ff90629fcf69bac0360f6d69e33b5081b174dd
feliciahsieh <felicia@tixwriteoff.com>
2018-01-23 01:43:16 +0000

    first version

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+include stdlib
+file{ '/var/www/html/wp-settings.php':
+  ensure => present,
+}->
+file_line{ 'replace':
+  path => '/var/www/html/wp-settings.php',
+  replace => true,
+  line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php');",
+  match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+}
